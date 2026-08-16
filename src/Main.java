4593bc117e737af05d5c9f907527b0f9d01a9705
Khazar Aliyev <aliyev.xezer@yahoo.com>
2026-08-16 14:25:30 +0400

    Refactor Main.java to handle email input

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,18 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
-
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+import java.util.Scanner;
+public class Khazar {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your email address: ");
+        String email = scanner.nextLine();
+        if(email.contains("@")){
+            int c = email.indexOf("@");
+            String username = email.substring(0,c);
+            String domain = email.substring(c+1);
+            System.out.printf("Your username is %s\n",username);
+            System.out.printf("Your domain is %s\n ",domain);
+        }
+        else{
+            System.out.println("Emails must contain @");
+        }
+    }
 }
